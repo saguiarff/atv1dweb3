020cdd95e9d282cd29ca3e331e191fc46f57c5b4
saguiarff <sofiaaguiarff@gmail.com>
2025-10-22 03:47:12 -0400

    03 - Cria objeto da aplicação Flask, adiciona rota /rota1 com a função principal e /rota2 com resposta em HTML

--- AppFlask.py
+++ AppFlask.py
@@ -1,17 +1,20 @@
-from flask import Flask
+from flask import Flask  
 
-app_Sofia = Flask (__name__)
+app_Sofia = Flask (__name__) 
 
-
-@app_Sofia.route('/')
-@app_Sofia.route('/ola')
-def raiz():
+@app_Sofia.route('/')   
+@app_Sofia.route('/rota1') 
+def rota1(): 
     return 'Olá!'
 
+@app_Sofia.route('/rota2')
+def rota2():
+    resposta = "<H3> Essa é outra página da rota 2 <H3>"
+    return resposta
 
-def saudacoes(nome):
-    return f'Olá, {nome}!'
-
+def saudacoes (nome): 
+    return f'Olá, {nome}'
 
 if __name__ == "__main__" :
-    app_Sofia.run(port = 8000)
+    app_Sofia.run(port = 8000) 
+                                                            
