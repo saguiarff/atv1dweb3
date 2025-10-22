af50f2154de6469527d3c430b7cf2e6e679cc9a0
saguiarff <sofiaaguiarff@gmail.com>
2025-10-22 15:44:05 -0400

    05 - duas novas rotas com páginas dinamicas com template html

--- AppFlask.py
+++ AppFlask.py
@@ -2,8 +2,7 @@ from flask import Flask, render_template
 
 app_Sofia = Flask(__name__ , template_folder='templates')
 
-
-@app_Sofia.route("/")  
+@app_Sofia.route("/")
 def homepage():          
     return render_template ("homepage.html")
 
@@ -11,5 +10,14 @@ def homepage():
 def contato():
     return render_template("contato.html") 
 
+@app_Sofia.route("/index")
+def indice():
+    return render_template ("index.html", nome = "Sofia") 
+
+@app_Sofia.route("/usuario")
+def dados_usuario():
+    dados_usu = {"nome": "Sofia", "ocupacao": "Estudante", "disciplina":"Desenvolvimento Web III"}
+    return render_template("usuario.html", dados = dados_usu)
+
 if __name__ == "__main__": 
-    app_Sofia.run(port = 8000)
+    app_Sofia.run(port = 8000) 
