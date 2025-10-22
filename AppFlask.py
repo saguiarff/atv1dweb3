b57fbf725700c8cfa520d853ef58079eae2663f8
saguiarff <sofiaaguiarff@gmail.com>
2025-10-22 03:11:54 -0400

    02 - criação de uma função de saudação, sem rota definida

--- AppFlask.py
+++ AppFlask.py
@@ -2,8 +2,16 @@ from flask import Flask
 
 app_Sofia = Flask (__name__)
 
+
 @app_Sofia.route('/')
+@app_Sofia.route('/ola')
 def raiz():
     return 'Olá!'
 
-app_Sofia.run()
+
+def saudacoes(nome):
+    return f'Olá, {nome}!'
+
+
+if __name__ == "__main__" :
+    app_Sofia.run(port = 8000)
