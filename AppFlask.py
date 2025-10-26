bdcc729e4f4d5059aefa9516c621155fa5cf08f5
saguiarff <sofiaaguiarff@gmail.com>
2025-10-26 04:23:27 -0400

    07 - os templates das rotas agora tem um template base, e foi adicionado a rota login

--- AppFlask.py
+++ AppFlask.py
@@ -1,31 +1,23 @@
 from flask import Flask, render_template
 
-app_Sofia = Flask(__name__ , template_folder='templates')
+app_Sofia = Flask(__name__, template_folder='templates') 
 
-@app_Sofia.route("/")
-def homepage():          
-    return render_template ("homepage.html", nome = "Sofia")
+@app_Sofia.route("/")       
+@app_Sofia.route("/index")  
+def indice():
+    return render_template ("index.html", nome = "Sofia") 
 
 @app_Sofia.route("/contato")
 def contato():
     return render_template("contato.html") 
 
-@app_Sofia.route("/index")
-def indice():
-    return render_template ("index.html", nome = "Sofia") 
+@app_Sofia.route("/login")
+def login():
+    return render_template("login.html") 
 
-@app_Sofia.route("/usuario")
-def dados_usuario():
-    dados_usu = {"nome": "Sofia", "ocupacao": "Estudante", "disciplina":"Desenvolvimento Web III"}
-    return render_template("usuario.html", dados = dados_usu)
-
-@app_Sofia.route('/usuario/<id>')
-def saudacao(id):
-    return render_template('homepage.html', nome=id)
-
-@app_Sofia.route("/usuario/<nome_usuario>/<nome_ocupacao>/<nome_disciplina>") 
-def usuario (nome_usuario, nome_ocupacao, nome_disciplina):  
-    dados_usu = {"ocupacao": nome_ocupacao, "disciplina": nome_disciplina}
+@app_Sofia.route("/usuario", defaults={"nome_usuario":"Sofia","nome_profissao":"Estudante"}) 
+def usuarios (nome_usuario, nome_profissao):
+    dados_usu = {"profissao": nome_profissao, "disciplina":"Desenvolvimento Web III"}
     return render_template ("usuario.html", nome=nome_usuario, dados = dados_usu)  
 
 if __name__ == "__main__": 
