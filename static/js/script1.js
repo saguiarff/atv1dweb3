965d10cb656991e325b85171105865dceac8fec1
saguiarff <sofiaaguiarff@gmail.com>
2025-10-27 16:28:03 -0400

    verifica se a senha contém pelo menos uma letra maiúscula, um número e um caractetere especial, e no mínimo 6 caracteres

--- static/js/script1.js
+++ static/js/script1.js
@@ -9,19 +9,20 @@ function validarSenha() {
     var senha = document.getElementById("senha").value;
 
     // Verificar o tamanho mínimo da senha
-    if (senha.length < 8) {
-        alert("A senha deve ter pelo menos 8 caracteres");
+    if (senha.length < 6) {
+        alert("A senha deve ter pelo menos 6 caracteres");
         return false;
     }
 
     // Verificar se a senha contém pelo menos maiúsula e números
     var uppercaseRegex = /[A-Z]/; //Regex é como uma máscara
     var numeroRegex = /[0-9]/;
-    //uma letra maiúscula e um número
-    if (!uppercaseRegex.test(senha) || !numeroRegex.test(senha)) {
-        alert("A senha deve conter pelo menos uma letra maiúscula e um número");
+    var especialRegex = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/;
+
+    // Verificar todas as condições
+    if (!uppercaseRegex.test(senha) || !numeroRegex.test(senha) || !especialRegex.test(senha)) {
+        alert("A senha deve conter pelo menos uma letra maiúscula, um número e um caractere especial");
         return false; // não passou na validação, retorna false 
     }
     return true; // o formulário será enviado para a rota /autenticar
 }
-
