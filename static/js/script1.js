07775a22f593c0df1c6cc7592dca64bb58c7ab97
saguiarff <sofiaaguiarff@gmail.com>
2025-10-27 21:48:14 -0400

    cadastro.html herdando de base.html, validação de senha e confirmação via javascript, formulário com Nome, CPF, E-mail, Telefone, Endereço e Senha e img tematica no cadastro

--- static/js/script1.js
+++ static/js/script1.js
@@ -1,28 +1,48 @@
 function verificarDigitacao(event) {
-    const fieldValue = event.target.value;
-    if (fieldValue.length > 20) {
+    if (event.target.value.length > 20) {
         alert("Texto muito longo!");
     }
-}
+    }
 
 function validarSenha() {
     var senha = document.getElementById("senha").value;
+    var uppercase = /[A-Z]/;
+    var numero = /[0-9]/;
+    var especial = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/;
 
-    // Verificar o tamanho mínimo da senha
     if (senha.length < 6) {
-        alert("A senha deve ter pelo menos 6 caracteres");
+        alert("A senha deve ter pelo menos 6 caracteres.");
         return false;
     }
-
-    // Verificar se a senha contém pelo menos maiúsula e números
-    var uppercaseRegex = /[A-Z]/; //Regex é como uma máscara
-    var numeroRegex = /[0-9]/;
-    var especialRegex = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/;
-
-    // Verificar todas as condições
-    if (!uppercaseRegex.test(senha) || !numeroRegex.test(senha) || !especialRegex.test(senha)) {
-        alert("A senha deve conter pelo menos uma letra maiúscula, um número e um caractere especial");
-        return false; // não passou na validação, retorna false 
+    if (!uppercase.test(senha) || !numero.test(senha) || !especial.test(senha)) {
+        alert("A senha deve conter pelo menos uma letra maiúscula, um número e um caractere especial.");
+        return false;
     }
-    return true; // o formulário será enviado para a rota /autenticar
+    return true;
+    }
+
+function validarCadastro() {
+  var senha = document.getElementById("senha").value;
+  var confirmar = document.getElementById("confirmar_senha").value;
+
+  if (senha !== confirmar) {
+    alert("As senhas não coincidem!");
+    return false;
+  }
+  return validarSenha();
+}
+
+function aplicarMascaraCPF() {
+  var cpf = document.getElementById("cpf");
+  cpf.value = cpf.value.replace(/\D/g, '')
+    .replace(/(\d{3})(\d)/, '$1.$2')
+    .replace(/(\d{3})(\d)/, '$1.$2')
+    .replace(/(\d{3})(\d{1,2})$/, '$1-$2');
+}
+
+function aplicarMascaraTelefone() {
+  var tel = document.getElementById("telefone");
+  tel.value = tel.value.replace(/\D/g, '')
+    .replace(/(\d{2})(\d)/, '($1) $2')
+    .replace(/(\d{5})(\d)/, '$1-$2');
 }
